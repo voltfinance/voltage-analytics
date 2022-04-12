5ffbb18635b989ad18b83d18e077b02769593b1e
diegofigs <12281609+diegofigs@users.noreply.github.com>
2022-04-12 16:14:47 -0400

    :green_heart: (pages/users) fix page build

--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -197,18 +197,4 @@ export async function getStaticProps({ params }) {
   };
 }
 
-export async function getStaticPaths() {
-  // const client = getApollo();
-
-  // const { data: { users } } = await client.query({
-  //   query: userIdsQuery,
-  // });
-
-  // const paths = users.map(user => ({
-  //   params: { id: user.id }
-  // }));
-
-  return { paths: [], fallback: true };
-}
-
 export default UsersPage;
