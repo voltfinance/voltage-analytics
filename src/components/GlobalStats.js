8a2f70794f34b089aa201d0afa17f471f6baafd9
diegofigs <12281609+diegofigs@users.noreply.github.com>
2022-04-19 22:59:28 -0400

    :sparkles:  add circ, total supply to global…

--- src/components/GlobalStats.js
+++ src/components/GlobalStats.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import { RowFixed, RowBetween } from './Row'
+import React, { useState } from "react";
+import { RowFixed, RowBetween } from "./Row";
 
-import { TYPE } from '../theme'
-import { makeStyles, useMediaQuery } from '@material-ui/core'
-import { formatCurrency } from 'app/core'
+import { TYPE } from "../theme";
+import { makeStyles, useMediaQuery } from "@material-ui/core";
+import { formatCurrency, formatDecimal } from "app/core";
+import useSupplyStats from "core/hooks/useSupplyStats";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
     position: "sticky",
@@ -13,45 +14,64 @@ const useStyles = makeStyles(theme => ({
   },
   medium: {
     fontWeight: 500,
-  }
+  },
 }));
-const localNumber = num => num;
+const localNumber = (num) => num;
 
 export default function GlobalStats(props) {
   const classes = useStyles();
 
-  const below1295 = useMediaQuery('(max-width: 1295px)')
-  const below1180 = useMediaQuery('(max-width: 1180px)')
-  const below1024 = useMediaQuery('(max-width: 1024px)')
-  const below816 = useMediaQuery('(max-width: 816px)')
+  const below1295 = useMediaQuery("(max-width: 1295px)");
+  const below1180 = useMediaQuery("(max-width: 1180px)");
+  const below1024 = useMediaQuery("(max-width: 1024px)");
+  const below816 = useMediaQuery("(max-width: 816px)");
+
+  const [{ circSupply, totalSupply }] = useSupplyStats();
 
   // const [showPriceCard, setShowPriceCard] = useState(false)
 
   const { oneDay, pairCount } = props.factory;
   const { totalVolumeUSD: oneDayVolumeUSD, txCount: oneDayTxns } = oneDay;
-  const oneDayFees = oneDayVolumeUSD ? formatCurrency(oneDayVolumeUSD * 0.003) : ''
+  const oneDayFees = oneDayVolumeUSD
+    ? formatCurrency(oneDayVolumeUSD * 0.003)
+    : "";
 
   return (
     <div className={classes.root}>
-      <RowBetween style={{ padding: below816 ? '0.5rem' : '.5rem' }}>
+      <RowBetween style={{ padding: below816 ? "0.5rem" : ".5rem" }}>
         <RowFixed>
+          {circSupply && totalSupply && (
+            <>
+              <TYPE.main mr="1rem">
+                Circ. Supply:{" "}
+                <span className={classes.medium}>{formatDecimal(circSupply)}</span>
+              </TYPE.main>
+              <TYPE.main mr="1rem">
+                Total Supply:{" "}
+                <span className={classes.medium}>{formatDecimal(totalSupply)}</span>
+              </TYPE.main>
+            </>
+          )}
           {!below1180 && (
-            <TYPE.main mr={'1rem'}>
-              Transactions (24H): <span className={classes.medium}>{localNumber(oneDayTxns)}</span>
+            <TYPE.main mr={"1rem"}>
+              Transactions (24H):{" "}
+              <span className={classes.medium}>{formatDecimal(oneDayTxns)}</span>
             </TYPE.main>
           )}
           {!below1024 && (
-            <TYPE.main mr={'1rem'}>
-              Pairs: <span className={classes.medium}>{localNumber(pairCount)}</span>
+            <TYPE.main mr={"1rem"}>
+              Pairs:{" "}
+              <span className={classes.medium}>{pairCount}</span>
             </TYPE.main>
           )}
           {!below1295 && (
-            <TYPE.main mr={'1rem'}>
-              Fees (24H): <span className={classes.medium}>{oneDayFees}</span>&nbsp;
+            <TYPE.main mr={"1rem"}>
+              Fees (24H): <span className={classes.medium}>{oneDayFees}</span>
+              &nbsp;
             </TYPE.main>
           )}
         </RowFixed>
       </RowBetween>
     </div>
-  )
+  );
 }
